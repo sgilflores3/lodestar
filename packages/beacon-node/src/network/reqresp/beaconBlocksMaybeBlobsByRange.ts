75b02974fb668536915e6eecd434e9d0efd76c8a
harkamal <gajinder@g11.in>
2023-03-19 18:21:48 +0530

    make blobsbyroot multi block

--- packages/beacon-node/src/network/reqresp/beaconBlocksMaybeBlobsByRange.ts
+++ packages/beacon-node/src/network/reqresp/beaconBlocksMaybeBlobsByRange.ts
@@ -1,6 +1,6 @@
 import {PeerId} from "@libp2p/interface-peer-id";
 import {BeaconConfig} from "@lodestar/config";
-import {deneb, Epoch, phase0} from "@lodestar/types";
+import {deneb, Epoch, phase0, allForks, Slot} from "@lodestar/types";
 import {ForkSeq} from "@lodestar/params";
 import {computeEpochAtSlot} from "@lodestar/state-transition";
 
@@ -38,23 +38,42 @@ export async function beaconBlocksMaybeBlobsByRange(
 
   // Only request blobs if they are recent enough
   else if (computeEpochAtSlot(startSlot) >= currentEpoch - config.MIN_EPOCHS_FOR_BLOB_SIDECARS_REQUESTS) {
-    const [blocks, allBlobSidecars] = await Promise.all([
+    const [allBlocks, allBlobSidecars] = await Promise.all([
       reqResp.beaconBlocksByRange(peerId, request),
       reqResp.blobSidecarsByRange(peerId, request),
     ]);
 
-    const blockInputs: BlockInput[] = [];
-    let blobSideCarIndex = 0;
-    let lastMatchedSlot = -1;
+    return matchBlockWithBlobs(config, allBlocks, allBlobSidecars, endSlot);
+  }
 
-    // Match blobSideCar with the block as some blocks would have no blobs and hence
-    // would be omitted from the response. If there are any inconsitencies in the
-    // response, the validations during import will reject the block and hence this
-    // entire segment.
-    //
-    // Assuming that the blocks and blobs will come in same sorted order
-    for (let i = 0; i < blocks.length; i++) {
-      const block = blocks[i];
+  // Post Deneb but old blobs
+  else {
+    throw Error("Cannot sync blobs outside of blobs prune window");
+  }
+}
+
+// Assumes that the blobs are in the same sequence as blocks, doesn't require block to be sorted 
+export function matchBlockWithBlobs(
+  config: BeaconConfig,
+  allBlocks: allForks.SignedBeaconBlock[],
+  allBlobSidecars: deneb.BlobSidecar[],
+  endSlot: Slot
+): BlockInput[] {
+  const blockInputs: BlockInput[] = [];
+  let blobSideCarIndex = 0;
+  let lastMatchedSlot = -1;
+
+  // Match blobSideCar with the block as some blocks would have no blobs and hence
+  // would be omitted from the response. If there are any inconsitencies in the
+  // response, the validations during import will reject the block and hence this
+  // entire segment.
+  //
+  // Assuming that the blocks and blobs will come in same sorted order
+  for (let i = 0; i < allBlocks.length; i++) {
+    const block = allBlocks[i];
+    if (config.getForkSeq(block.message.slot) < ForkSeq.deneb) {
+      blockInputs.push(getBlockInput.preDeneb(config, block));
+    } else {
       const blobSidecars: deneb.BlobSidecar[] = [];
 
       let blobSidecar: deneb.BlobSidecar;
@@ -74,27 +93,22 @@ export async function beaconBlocksMaybeBlobsByRange(
 
       blockInputs.push(getBlockInput.postDeneb(config, block, blobSidecars));
     }
-
-    // If there are still unconsumed blobs this means that the response was inconsistent
-    // and matching was wrong and hence we should throw error
-    if (
-      allBlobSidecars[blobSideCarIndex] !== undefined &&
-      // If there are no blobs, the blobs request can give 1 block outside the requested range
-      allBlobSidecars[blobSideCarIndex].slot <= endSlot
-    ) {
-      throw Error(
-        `Unmatched blobSidecars, blocks=${blocks.length}, blobs=${
-          allBlobSidecars.length
-        } lastMatchedSlot=${lastMatchedSlot}, pending blobsSidecars slots=${allBlobSidecars
-          .slice(blobSideCarIndex)
-          .map((blb) => blb.slot)}`
-      );
-    }
-    return blockInputs;
   }
 
-  // Post Deneb but old blobs
-  else {
-    throw Error("Cannot sync blobs outside of blobs prune window");
+  // If there are still unconsumed blobs this means that the response was inconsistent
+  // and matching was wrong and hence we should throw error
+  if (
+    allBlobSidecars[blobSideCarIndex] !== undefined &&
+    // If there are no blobs, the blobs request can give 1 block outside the requested range
+    allBlobSidecars[blobSideCarIndex].slot <= endSlot
+  ) {
+    throw Error(
+      `Unmatched blobSidecars, blocks=${allBlocks.length}, blobs=${
+        allBlobSidecars.length
+      } lastMatchedSlot=${lastMatchedSlot}, pending blobsSidecars slots=${allBlobSidecars
+        .slice(blobSideCarIndex)
+        .map((blb) => blb.slot)}`
+    );
   }
+  return blockInputs;
 }
--- packages/beacon-node/src/network/reqresp/beaconBlocksMaybeBlobsByRoot.ts
+++ packages/beacon-node/src/network/reqresp/beaconBlocksMaybeBlobsByRoot.ts
@@ -1,37 +1,35 @@
 import {PeerId} from "@libp2p/interface-peer-id";
 import {BeaconConfig} from "@lodestar/config";
 import {phase0, deneb} from "@lodestar/types";
-import {toHex} from "@lodestar/utils";
-import {BlockInput, getBlockInput} from "../../chain/blocks/types.js";
+import {BlockInput} from "../../chain/blocks/types.js";
 import {IReqRespBeaconNode} from "./interface.js";
+import {matchBlockWithBlobs} from "./beaconBlocksMaybeBlobsByRange.js";
 
 export async function beaconBlocksMaybeBlobsByRoot(
   config: BeaconConfig,
   reqResp: IReqRespBeaconNode,
   peerId: PeerId,
   request: phase0.BeaconBlocksByRootRequest
-  // currentSlot: Epoch,
-  // finalizedSlot: Slot
 ): Promise<BlockInput[]> {
-  // Assume all requests are post Deneb
-  // TODO: make this multiblock
-  const [blockRoot] = request;
-  const resBlocks = await reqResp.beaconBlocksByRoot(peerId, request);
-  if (resBlocks.length < 1) {
-    throw Error(`beaconBlocksByRoot return empty for block root ${toHex(blockRoot)}`);
-  }
-  const blockInputs = [];
-  for (const block of resBlocks) {
-    const blobKzgCommitmentsLen = (block.message.body as deneb.BeaconBlockBody).blobKzgCommitments.length ?? 0;
+  const allBlocks = await reqResp.beaconBlocksByRoot(peerId, request);
+  const blobIdentifiers: deneb.BlobIdentifier[] = [];
 
-    // TODO: freetheblobs
-    const blobSidecars = await Promise.all(
-      Array.from({length: blobKzgCommitmentsLen}, (_v, index) => {
-        const blobRequest = [{blockRoot, index}];
-        return reqResp.blobSidecarsByRoot(peerId, blobRequest).then((response) => response[0]);
-      })
-    );
-    blockInputs.push(getBlockInput.postDeneb(config, block, blobSidecars));
+  for (const block of allBlocks) {
+    const blockRoot = config.getForkTypes(block.message.slot).BeaconBlock.hashTreeRoot(block.message);
+    const blobKzgCommitmentsLen = (block.message.body as deneb.BeaconBlockBody).blobKzgCommitments.length ?? 0;
+    for (let index = 0; index < blobKzgCommitmentsLen; index++) {
+      blobIdentifiers.push({blockRoot, index});
+    }
   }
-  return blockInputs;
+
+  let allBlobSidecars: deneb.BlobSidecar[];
+  if (blobIdentifiers.length > 0) {
+    allBlobSidecars = await reqResp.blobSidecarsByRoot(peerId, blobIdentifiers);
+  } else {
+    allBlobSidecars = [];
+  }
+
+  // The last arg is to provide slot to which all blobs should be exausted in matching
+  // and here it should be infinity since all bobs should match
+  return matchBlockWithBlobs(config, allBlocks, allBlobSidecars, Infinity);
 }
